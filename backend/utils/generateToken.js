986dc0fb3a7495113a2c90dbe99ac3827a866844
Tanzil ansari <tanzilansari207@gmail.com>
2025-08-15 23:31:20 +0530

    removing all errors

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id, res) => {
-  const token = jwt.sign({ id }, process.env.JWT_SEC, {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
 
